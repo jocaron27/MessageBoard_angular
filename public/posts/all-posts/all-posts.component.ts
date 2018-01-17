029253b95d976cdd9dca0dbb8b39e7c464e2c68d
Jeanne Castillo <jeanne@cedrus.digital>
2018-01-17 09:25:11 -0500

    feat(posts) simplify single posts

--- public/posts/all-posts/all-posts.component.ts
+++ public/posts/all-posts/all-posts.component.ts
@@ -29,3 +29,37 @@
         controller: ["$http", "$state", "fetchPosts", controller]
     })
 }());
+
+
+// namespace app {
+//     'use strict'
+
+//     // function fetchPosts($http: angular.IHttpService) {
+//     //     return $http.get("/api/posts")
+//     //     .then(res => res.data);
+//     // }
+
+//     class postsController{
+//         constructor($http: angular.IHttpService, $state: angular.) {
+//         }
+        
+//         posts = [];
+
+//         $onInit = function() {
+//             fetchPosts().then(posts => {
+//                 this.posts = posts;
+//             })
+//         }
+
+//         goTo = function(id) {
+//             $state.go("post", {id})
+//         }
+//     }
+
+//     angular.module("app.posts")
+//     .component("allPosts", {
+//         templateUrl: "/posts/all-posts/all-posts.component.html",
+//         controllerAs: "vm",
+//         controller: ["$http", "$state", "fetchPosts", postsController]
+//     })
+// }
